00f88907c940093a74512a57c9ad997ff4073477
YouqiYang <125765840+YouqiYang@users.noreply.github.com>
2023-07-20 15:02:07 -0400

    Add files via upload

---
+++ Environmental/COMBINED_BME_WIFI/COMBINED_BME_WIFI.ino
@@ -0,0 +1,138 @@
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "arduino_secret.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+unsigned long delayTime;
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char* mqtt_username = "aws_server";
+const char* mqtt_password = "esports_data";
+
+const char broker[] = "ec2-18-222-166-18.us-east-2.compute.amazonaws.com";
+int        port     = 8883;
+const char topic[]  = "Pressure";
+const char topic2[]  = "Approx. Altitude";
+const char topic3[]  = "Humidity";
+
+//set interval for sending messages (milliseconds)
+const long interval = 1000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  mqttClient.setUsernamePassword(mqtt_username, mqtt_password);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  //下面是BME280部分
+  Serial.println(F("BME280 test"));
+
+  bool status;
+  
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin();  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+
+  Serial.println();
+}
+
+void loop() {
+  // call poll() regularly to allow the library to send MQTT keep alive which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    //record random value from A0, A1 and A2
+    int Rvalue = bme.readPressure() / 100.0F;
+    int Rvalue2 = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    int Rvalue3 = bme.readHumidity();
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(Rvalue);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic2);
+    Serial.println(Rvalue2);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic3);
+    Serial.println(Rvalue3);
+
+    // send message, the Print interface can be used to set the message contents
+    mqttClient.beginMessage(topic);
+    mqttClient.print(Rvalue);
+    mqttClient.endMessage();
+
+    mqttClient.beginMessage(topic2);
+    mqttClient.print(Rvalue2);
+    mqttClient.endMessage();
+
+    mqttClient.beginMessage(topic3);
+    mqttClient.print(Rvalue3);
+    mqttClient.endMessage();
+
+    Serial.println();
+  }
+}
---
+++ Environmental/Python_Read_Serial/Read_Serial.py
@@ -0,0 +1,16 @@
+import serial
+
+# Define the serial port and baud rate (make sure to match the one in your Arduino sketch)
+ser = serial.Serial('COM6', 9600)  # Replace 'COMx' with the appropriate serial port (e.g., 'COM3' on Windows, '/dev/ttyUSB0' on Linux)
+
+# Open a text file for writing the data
+file_path = 'data.txt'
+
+try:
+    with open(file_path, 'w') as file:
+        while True:
+            data = ser.readline().decode().strip()  # Read a line of data from the serial port and decode it
+            print(data)  # Print the received data
+            file.write(data + '\n')  # Write the data to the text file
+except KeyboardInterrupt:
+    ser.close()  # Close the serial port on Ctrl+C
