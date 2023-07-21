9c12b44d2e5d4576029cf4e2a6d948f9883b2f63
LCJ-CMU <131792207+LCJ-CMU@users.noreply.github.com>
2023-07-21 16:04:02 -0400

    Add files via upload

--- OCR/ocr_mp.py
+++ OCR/ocr_mp.py
@@ -17,7 +17,6 @@ weapon_list = ['AWP', 'G3SG1', 'Galil AR', 'M4A4', 'M4A1-S', 'SCAR-20', 'SG 553'
 # average frame reading time: 0.0014 sec
 # average ocr processing time: 0.1 sec
 
-
 def read_frames(frame_queue, index):
     # this function will read frames from the video
     time_list = []
@@ -38,8 +37,9 @@ def read_frames(frame_queue, index):
         index += 1
         if index == 1000:
             end = time.time()
+            dat = datetime.fromtimestamp(end)
             frame_queue.put(None)  # signal that we are done reading frames
-            print(end-sta)
+            print('streaming process end in: {}'.format(dat.strftime('%Y-%m-%d-%H-%M-%S-') + f'{dat.microsecond // 1000:03}'))
             # print("Reading frame time is ")
             # print(time_list)
             break
@@ -50,19 +50,22 @@ def read_frames(frame_queue, index):
 
 
 def process_frame(frame_queue):
-    reader = easyocr.Reader(['en'], gpu=True)  # Initialize the EasyOCR reader with desired languages
+    # Initiate OCR Engine
+    reader = easyocr.Reader(['en'], gpu=True) 
+    # --------------------------------------------------------Player Status--------------------------------------------------------#
     data_dict = {}
     hp_ac = []
-    updated_killers = []
-    updated_killees = []
-    sta = time.time()
-    dat = datetime.fromtimestamp(sta)
-
+    player_status = 1 # Live -> 1  Death -> 0
     last_hp = 100
     last_ac = 100
 
+    sta = time.time()
+    dat = datetime.fromtimestamp(sta)
     print('start processing:{}'.format(dat.strftime('%Y-%m-%d-%H-%M-%S-') + f'{dat.microsecond // 1000:03}'))
+    # --------------------------------------------------------Player Status--------------------------------------------------------#
     while True:
+
+        # -------------------------------------------------------image load--------------------------------------------------------#
         image = frame_queue.get()  # get a frame from the queue
         if image is None:
             break
@@ -70,12 +73,24 @@ def process_frame(frame_queue):
         ctime = time.time()
             # Do image processing here
         # if index % 600 == 0:
-        name = 'result/' +  str(ctime) + '.png'
+        name = 'result/Frame' +  str(ctime) + '.png'
         cv2.imwrite(name, image)
         # name = 'result/Frame'+str(index)+'.png'
         # cv2.imwrite(name, image)
         height, width, _ = image.shape
+        data_dict[str(ctime)] = {}
+        # -------------------------------------------------------image load--------------------------------------------------------#
 
+        # ----------------------------------------------------------score----------------------------------------------------------#
+        scores = image[int(40 * height / 1440):int(75 * height / 1440),int(1222 * width / 2560):int(1338 * width / 2560)]
+        results = reader.readtext(scores, allowlist='0123456789', paragraph=True, contrast_ths=0.4)
+        score = ' '.join([result[1] for result in results])
+        data_dict[str(ctime)]['score'] = score
+        # name = 'result/'+str(ctime)+'.png'
+        # cv2.imwrite(name, scores)
+        # ----------------------------------------------------------score----------------------------------------------------------#
+
+        # ---------------------------------------------------------weapon----------------------------------------------------------#
         part1_weap1 = image[int(906 * height / 1440):int(925 * height / 1440),
                     int(2100 * width / 2560):int(2530 * width / 2560)]  # top
         part1_weap2 = image[int(1012 * height / 1440):int(1042 * height / 1440),
@@ -96,7 +111,6 @@ def process_frame(frame_queue):
         results = reader.readtext(part1_concate, allowlist='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-',
                                 paragraph=True, batch_size=8)
         
-        data_dict[str(ctime)] = {}
         weapon_name = ' '.join([result[1] for result in results])
 
         if weapon_name != "":
@@ -110,18 +124,20 @@ def process_frame(frame_queue):
         
         else:
             data_dict[str(ctime)]['weapon'] = ""
-        # HP and AC
-        
-        part2_hp = cv2.cvtColor(image[int(1399 * height / 1440):int(1413 * height / 1440),int(166 * width / 2560):int(273 * width / 2560)], cv2.COLOR_BGR2GRAY)
-        part2_hp = cv2.inRange(part2_hp, 80, 160)
-        part2_hp = cv2.bitwise_not(part2_hp)
-        hp = int(np.round((100*np.count_nonzero(part2_hp[0] == 255)/107))/(width / 2560))
+        # ---------------------------------------------------------weapon---------------------------------------------------------#
+
+        # -------------------------------------------------------HP and AC--------------------------------------------------------#
+        part2_hp = image[int(1399 * height / 1440):int(1413 * height / 1440),int(166 * width / 2560):int(273 * width / 2560)]
+        b, g, r = cv2.split(part2_hp)
+        r[r > 180] = 255
+        r[r <= 180] = 0
+        hp = int(np.round((100*np.count_nonzero(r[5] == 255)/107))/(width / 2560))
         if hp > last_hp:
             hp = last_hp
         else:
             last_hp = hp
-        part2_ac = cv2.cvtColor(image[int(1399 * height / 1440):int(1413 * height / 1440),int(442 * width / 2560):int(549 * width / 2560)], cv2.COLOR_BGR2GRAY)
-        part2_ac = cv2.inRange(part2_ac, 80, 160)
+        part2_ac = image[int(1399 * height / 1440):int(1413 * height / 1440),int(442 * width / 2560):int(549 * width / 2560)]
+        part2_ac = cv2.inRange(part2_ac, 0, 160)
         part2_ac = cv2.bitwise_not(part2_ac)
         ac = int(np.round((100*np.count_nonzero(part2_ac[0] == 255)/107))/(width / 2560))
         if ac > last_ac:
@@ -146,15 +162,6 @@ def process_frame(frame_queue):
         # results_ac = reader.readtext(part2_ac, allowlist='0123456789',
         #                         paragraph=True)
         
-
-        # part2_hp = cv2.inRange(part2_hp, 80, 160)
-        # part2_hp = cv2.bitwise_not(part2_hp)
-        # hp = int(np.round((100*np.count_nonzero(part2_hp[0] == 255)/107))/(width / 2560))
-        # part2_ac = cv2.cvtColor(image[int(1399 * height / 1440):int(1413 * height / 1440),int(442 * width / 2560):int(549 * width / 2560)], cv2.COLOR_BGR2GRAY)
-        # part2_ac = cv2.inRange(part2_ac, 80, 160)
-        # part2_ac = cv2.bitwise_not(part2_ac)
-        # ac = int(np.round((100*np.count_nonzero(part2_ac[0] == 255)/107))/(width / 2560))
-
         # # name = 'result/' + str(ctime) + 'part2.png'
         # # cv2.imwrite(name, part2_hp)
         
@@ -164,8 +171,9 @@ def process_frame(frame_queue):
         # else:
         #     hp_ac.append([ctime, results_hp[0][1], results_ac[0][1]])
         #     data_dict[str(ctime)]['hp_ac'] = [results_hp[0][1], results_ac[0][1]]
+        # -------------------------------------------------------HP and AC--------------------------------------------------------#
 
-
+        # ---------------------------------------------------------Money----------------------------------------------------------#
         part3_money = image[int(470 * height / 1440):int(530 * height / 1440),
                     int(60 * width / 2560):int(190 * width / 2560)]
 
@@ -176,7 +184,9 @@ def process_frame(frame_queue):
 
         money = ' '.join([result[1] for result in results])
         data_dict[str(ctime)]['money'] = money
+        # ---------------------------------------------------------Money----------------------------------------------------------#
 
+        # --------------------------------------------------------Magazine--------------------------------------------------------#
         part4_magaz = image[int(1382 * height / 1440):int(1420 * height / 1440),
                     int(2280 * width / 2560):int(2445 * width / 2560)]
 
@@ -185,10 +195,9 @@ def process_frame(frame_queue):
         
         magaz = ' '.join([result[1] for result in results])
         data_dict[str(ctime)]['magaz'] = magaz
-        name = 'result/' + str(ctime) + 'part4.png'
-        cv2.imwrite(name, part4_magaz)
-        # print(' '.join([result[1] for result in results]))
+        # --------------------------------------------------------Magazine--------------------------------------------------------#
 
+        # ----------------------------------------------------------Kill----------------------------------------------------------#
         part5_kill1 = image[int(100 * height / 1440):int(143 * height / 1440),
                     int(2000 * width / 2560):int(2550 * width / 2560)]
         part5_kill2 = image[int(147 * height / 1440):int(190 * height / 1440),
@@ -207,19 +216,13 @@ def process_frame(frame_queue):
         killees = []
         previous = 0
 
-        # cv2.imwrite("part1.png", part1_concate)
-        # cv2.imwrite("part2.png", part2_hp)
-        # cv2.imwrite("part3.png", part3_money)
-        # cv2.imwrite("part4.png", part4_magaz)
-        # cv2.imwrite("part5.png", part5_concate)
-        # cv2.imwrite("image.png", image)
-        
-        results = reader.readtext(part5_concate, allowlist='+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ', batch_size=16)
+        results = reader.readtext(part5_concate, allowlist='+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ', batch_size=16, width_ths=1)
         # text = ' '.join([result[1] for result in results])
-
-
+        
         i = 0
+        print(ctime, ":")
         for result in results:
+            print(result[1])
             if i % 2 == 0:
                 if len(result[1]) <= 2:
                     i = i - 1
@@ -231,62 +234,32 @@ def process_frame(frame_queue):
                     continue
                 killees.append(result[1])
             i = i + 1
-
-
         
-        # for j in range(len(killers)):
-        #     split_killers = killers[j].split()
 
-        #     closest_string_killer = ''
-        #     closest_string_killee = ''
-        #     count = 0
 
-            
-        #     for split_killer in split_killers:
-        #         if split_killer.lower() == 'bot':
-        #             count = count + 1
-        #             continue
-        #         distances = [Levenshtein.distance(split_killer, target) for target in list]
-        #         min_distance_index = distances.index(min(distances))
-        #         closest_string_killer = list[min_distance_index]
-
-        #         if count >= 2:
-        #             closest_string_killer += " and "
-        #         closest_string_killer += list[min_distance_index]
-            
-
-        #     split_killees = killees[j].split()
-        #     for split_killee in split_killees:
-        #         if split_killee.lower() == 'bot':
-        #             continue
-
-        #         distances = [Levenshtein.distance(split_killee, target) for target in list]
-        #         min_distance_index = distances.index(min(distances))
-        #         closest_string_killee = list[min_distance_index]
-
-        #         if "athletechyinzcam" in closest_string_killee or "athletechyinzcam" in closest_string_killer:
-        #             updated_killers.append(closest_string_killer)
-        #             updated_killees.append(closest_string_killee)
         closest_string_killer = ''
         closest_string_killee = ''
+        updated_killers = []
+        updated_killees = []
 
         for killer in killers:
 
             split_killers = killer.split()
-            count = 0
+            # count = 0
 
             
             for split_killer in split_killers:
                 if split_killer.lower() == 'bot':
-                    count = count + 1
+                    # count = count + 1
                     continue
                 distances = [Levenshtein.distance(split_killer, target) for target in list]
                 min_distance_index = distances.index(min(distances))
                 closest_string_killer = list[min_distance_index]
 
-                if count >= 2:
-                    closest_string_killer += " and "
-                closest_string_killer += list[min_distance_index]
+                # if count >= 2:
+                #     closest_string_killer += " and "
+                # closest_string_killer += list[min_distance_index]
+                updated_killers.append(closest_string_killer)
             
 
         for killee in killees:
@@ -298,41 +271,29 @@ def process_frame(frame_queue):
                 distances = [Levenshtein.distance(split_killee, target) for target in list]
                 min_distance_index = distances.index(min(distances))
                 closest_string_killee = list[min_distance_index]
+                updated_killees.append(closest_string_killee)
+        
+        kill_list = []
+        for k in range(min(len(updated_killees), len(updated_killers))):
 
-        if "athletechyinzcam" in closest_string_killee or "athletechyinzcam" in closest_string_killer:
-            updated_killers.append(closest_string_killer)
-            updated_killees.append(closest_string_killee)
+            if "athletechyinzcam" in updated_killers[k] or "athletechyinzcam" in updated_killees[k]:
+                kill_list.append({updated_killers[k] : updated_killees[k]})
+                data_dict[str(ctime)]['kill_info'] = kill_list
 
-
-    kill_list = []
-
-    for i in range(len(updated_killees)):
-        kill_list.append({updated_killers[i]:updated_killees[i]})
-    data_dict[str(ctime)]['kill_info'] = kill_list
-
-
-        # print("\n\n********Next Image")
-
-        # Exit if ESC key is pressed
-        # if cv2.waitKey(20) & 0xFF == 27:
-        # break
+        # ----------------------------------------------------------Kill----------------------------------------------------------#
     end = time.time()
-    print(end-sta)
+    dat = datetime.fromtimestamp(end)
+    print('process end in: {}'.format(dat.strftime('%Y-%m-%d-%H-%M-%S-') + f'{dat.microsecond // 1000:03}'))
     np.save('hp_record.npy', np.array(hp_ac))
-    # print(updated_killers)
-    # print(updated_killees)
     with open('data.json', 'w') as f:
     # Use json.dump to write dict_data to the file
         json.dump(data_dict, f, indent=4)
-# When everything done, release the VideoCapture object
 
-# end = time.time()
-# print(sta-end)
-# Close all the frames
+
 if __name__ == '__main__':
     index = 0
     frame_queue = Queue()
-
+ 
     # create two processes
     p1 = Process(target=read_frames, args=(frame_queue, index,))
     p2 = Process(target=process_frame, args=(frame_queue,))
