ad2b52e48debb04e48abdcb4eac3c5f89906b96d
Tonychen050400 <61198124+Tonychen050400@users.noreply.github.com>
2023-07-26 13:46:44 -0400

    Add files via upload
    detects the posture change

---
+++ Camera/posture2.py
@@ -0,0 +1,84 @@
+import cv2
+import mediapipe as mp
+import matplotlib.pyplot as plt
+
+# Create a MediaPipe Pose object
+mp_pose = mp.solutions.pose
+
+video_capture = cv2.VideoCapture(0)
+frame_rate = video_capture.get(cv2.CAP_PROP_FPS)
+print(f"Frame rate: {frame_rate} fps")
+
+# Initialize previous landmark positions
+prev_landmarks = None
+
+# Create lists to store the differences in landmark positions
+diff_x_list = []
+diff_y_list = []
+
+# Threshold to filter out erroneous differences
+threshold = 0.0005 # Adjust this value as needed
+
+with mp_pose.Pose() as pose:
+    while True:
+        ret, frame = video_capture.read()
+
+        if not ret:
+            print("Error reading frame from video stream")
+            break
+
+        # Convert the frame to RGB format
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame with MediaPipe Pose
+        results = pose.process(frame_rgb)
+
+        # Extract landmarks from the current frame
+        landmarks = []
+        if results.pose_landmarks:
+            for landmark in results.pose_landmarks.landmark:
+                landmarks.append((landmark.x, landmark.y, landmark.z))
+
+        # Compare with previous frame
+        if prev_landmarks and landmarks:
+            if len(landmarks) >= 2:
+                diff_x = landmarks[0][0] - prev_landmarks[0][0]
+                diff_y = landmarks[1][1] - prev_landmarks[1][1]
+                if abs(diff_x) > threshold and abs(diff_y) > threshold:
+                    diff_x_list.append(diff_x)
+                    diff_y_list.append(diff_y)
+        
+        # Update the previous landmarks
+        prev_landmarks = landmarks
+
+        # Visualize the pose landmarks on the frame
+        if results.pose_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(
+                frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS
+            )
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
+# Plot the changes in the posture
+frame_count = len(diff_x_list)
+x = range(frame_count)
+duration_seconds = frame_count / frame_rate
+print(f"Duration: {duration_seconds} seconds")
+
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
+ax1.plot(x, diff_x_list, label='X Coordinate Difference')
+ax1.set_ylabel('X Coordinate Difference')
+
+ax2.plot(x, diff_y_list, label='Y Coordinate Difference')
+ax2.set_ylabel('Y Coordinate Difference')
+ax2.set_xlabel('Frame')
+
+plt.suptitle('Changes in Posture over Frames')
+plt.show()
